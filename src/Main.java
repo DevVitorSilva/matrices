69694691721f254cedc460b50e8bbb2da104d4b1
João Vitor <joao@localhost.localdomain>
2024-02-14 15:11:00 -0300

    create variables and loops to solve the problem

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,25 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        
+        System.out.print("Enter a length for the array: ");
+        int length = sc.nextInt();
+        int[][] array = new int[length][length];
+        int[] mainDiagonal = new int[length];
+        int negativeNumber = 0;
+        for(int i = 0; i < length; i++){
+            for(int i2 = 0; i2 < length; i2++){
+                array[i][i2] = sc.nextInt();
+                if(array[i][i2] < 0){
+                    negativeNumber++;
+                }
+            }
+            mainDiagonal[i] = array[i][i];
+        }
+        System.out.println("Main diagonal:");
+        for(int diagonal : mainDiagonal){
+            System.out.print(diagonal + " ");
+        }
+        System.out.println("\nNegative numbers: " + negativeNumber);
         sc.close();
     }
 }
